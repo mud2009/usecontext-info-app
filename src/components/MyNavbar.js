50eafd99e9a0bb73b0eb7151a6caa08da1d31a74
Will Greenberg <greenberg.will@gmail.com>
2022-05-11 14:55:55 -0700

    use refs to change email in navbar

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -5,17 +5,20 @@ import { useAuth } from '../contexts/AuthContext'
 export default function MyNavbar() {
   const { currentUser } = useAuth()
   let userDisplay = useRef("Log In")
+  let userRoute = useRef("/login")
   if(currentUser){
     userDisplay = currentUser.email
+    userRoute = "/profile"
   } else {
     userDisplay = "Log In"
+    userRoute = "/login"
   }
   return (
     <div className='ml-2 mr-2'>
       <Navbar className='justify-content-between'>
           <Navbar.Brand href="/">Info Feed</Navbar.Brand>
           <Nav className='flex-grow'>
-            <Nav.Link href="/login">{userDisplay}</Nav.Link>
+            <Nav.Link href={userRoute}>{userDisplay}</Nav.Link>
           </Nav>
       </Navbar>
     </div>

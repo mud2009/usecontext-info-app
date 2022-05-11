1759fdbc4e92e5fa21a9100bba55187175340d46
Will Greenberg <greenberg.will@gmail.com>
2022-05-11 14:55:29 -0700

    move logout to to profile

--- src/components/Control.js
+++ src/components/Control.js
@@ -4,18 +4,8 @@ import {useAuth } from "../contexts/AuthContext"
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Control() {
-  const [ error, setError ] = useState("")
   const { currentUser, logOut } = useAuth()
   const history = useNavigate()
-  async function handleLogOut(){
-    setError("")
-    try {
-      await logOut()
-      history.push("/login")
-    } catch {
-      setError("Could not Log Out")
-    }
-  }
   if (!currentUser){
     return(
       <>
@@ -30,17 +20,6 @@ export default function Control() {
   }
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="update-profile" className='btn btn-primary w-100 mt-3'>Update Profile</Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant='link' onClick={handleLogOut}>Log Out</Button>
-      </div>
     </>
     )
 }

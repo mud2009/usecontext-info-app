c95d50cc0c480351addce514363ff4f45c8ad5b3
Will Greenberg <greenberg.will@gmail.com>
2022-05-11 14:45:14 -0700

    add conditional rendering to navbar if someone is logged in

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useRef} from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function MyNavbar() {
+  const { currentUser } = useAuth()
+  let userDisplay = useRef("Log In")
+  if(currentUser){
+    userDisplay = currentUser.email
+  } else {
+    userDisplay = "Log In"
+  }
   return (
-    <div className='pl-2 pr-2'>
+    <div className='ml-2 mr-2'>
       <Navbar className='justify-content-between'>
           <Navbar.Brand href="/">Info Feed</Navbar.Brand>
           <Nav className='flex-grow'>
-            <Nav.Link href="/login">Log In</Nav.Link>
+            <Nav.Link href="/login">{userDisplay}</Nav.Link>
           </Nav>
       </Navbar>
     </div>

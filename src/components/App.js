8db5688d79168f9f57d4b5d5539ccaac6c7ae076
Will Greenberg <greenberg.will@gmail.com>
2022-05-11 14:43:59 -0700

    wrap navbar in authprovider

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,28 @@ import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
-import Dashboard from "./Dashboard";
+import Control from "./Control";
 import MyNavbar from "./MyNavbar";
 import { AuthProvider } from "./../contexts/AuthContext"
 
 function App() {
   return (
     <>
-      <MyNavbar />
-      <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "80vh" }}>
-        <div className="w-100" style={{maxWidth: '400px'}}>
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route exact path="/" element={<Dashboard/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/login" element={<LogIn/>}/>
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
+      <AuthProvider>
+        <MyNavbar />
+        <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "80vh" }}>
+          <div className="w-100" style={{maxWidth: '400px'}}>
+            <Router>
+                <Routes>
+                  <Route exact path="/" element={<Control/>}/>
+                  <Route path="/signup" element={<SignUp/>}/>
+                  <Route path="/login" element={<LogIn/>}/>
+                </Routes>
+            </Router>
+          </div>
+        </Container>
+      </AuthProvider>
     </>
   );
 }
